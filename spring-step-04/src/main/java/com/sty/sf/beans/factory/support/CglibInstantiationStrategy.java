59ce306ca46ddbae0d675ac3dfb908db55b711ab
tianma <tianma@bytenew.com>
2022-06-25 15:51:58 +0800

    手撸spring-属性注入和依赖

---
+++ spring-step-04/src/main/java/com/sty/sf/beans/factory/support/CglibInstantiationStrategy.java
@@ -0,0 +1,34 @@
+package com.sty.sf.beans.factory.support;
+
+import com.sty.sf.beans.BeansException;
+import com.sty.sf.beans.factory.config.BeanDefinition;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Cglib实例化
+ *
+ * @author tianma
+ * @date 2022/6/22
+ * @version 1.0.0
+ */
+public class CglibInstantiationStrategy implements InstantiationStrategy {
+
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor constructor, Object[] args) throws BeansException {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(beanDefinition.getBeanClass());
+        enhancer.setCallback(new NoOp() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        });
+        if (constructor == null) {
+            enhancer.create();
+        }
+        return enhancer.create(constructor.getParameterTypes(), args);
+    }
+}
---
+++ spring-step-04/src/main/java/com/sty/sf/beans/factory/support/JdkInstantiationStrategy.java
@@ -0,0 +1,30 @@
+package com.sty.sf.beans.factory.support;
+
+import com.sty.sf.beans.BeansException;
+import com.sty.sf.beans.factory.config.BeanDefinition;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * JDK实例化
+ *
+ * @author tianma
+ * @date 2022/6/22
+ * @version 1.0.0
+ */
+public class JdkInstantiationStrategy implements InstantiationStrategy{
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor constructor, Object[] args) throws BeansException {
+        Class<?> beanClass = beanDefinition.getBeanClass();
+        try {
+            if (constructor == null) {
+                return beanClass.getDeclaredConstructor().newInstance();
+            }else {
+                return beanClass.getDeclaredConstructor(constructor.getParameterTypes()).newInstance(args);
+            }
+        } catch (NoSuchMethodException | InstantiationException | InvocationTargetException | IllegalAccessException e) {
+            throw new BeansException("",e);
+        }
+    }
+}
